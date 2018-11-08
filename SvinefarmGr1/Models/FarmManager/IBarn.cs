2697b4b938d5aa1e31fd2db783f74c50c1785b16
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2018-11-08 11:26:47 +0100

    Barn interface added

---
+++ SvinefarmGr1/Models/FarmManager/IBarn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SvinefarmGr1.Models.FarmManager
+{
+    public interface IBarn
+    {
+        int BarnId { get; set; }
+        string BarnName { get; set; }
+        ICollection<Box> Boxes { get; set; }
+        Farm Farm { get; set; }
+    }
+}
