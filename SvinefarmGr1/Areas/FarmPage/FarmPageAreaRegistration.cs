3da6bed3a88622f097d1fca051ac918513adb282
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2018-11-08 13:09:47 +0100

    Added farmcontroller API

---
+++ SvinefarmGr1/Areas/FarmPage/FarmPageAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SvinefarmGr1.Areas.FarmPage
+{
+    public class FarmPageAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "FarmPage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "FarmPage_default",
+                "FarmPage/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SvinefarmGr1/Controllers/API/FarmController.cs
@@ -0,0 +1,46 @@
+﻿using SvinefarmGr1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SvinefarmGr1.Controllers.API
+{
+    public class FarmController : ApiController
+    {
+        // GET: api/Farm
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var farms = context.Farms.ToList();
+                return Ok(farms);
+            }
+
+        }
+
+        // GET: api/Farm/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Farm
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Farm/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Farm/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
