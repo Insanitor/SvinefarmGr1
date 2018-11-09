f0427112ceb6cae798fe2f5262f64c7217982d01
Martin Juul Johansen <zbcmajoh57@zbc.dk>
2018-11-09 13:28:26 +0100

    Added Angular to the project.. Added view + angular js controller that allow us to create Farms.

---
+++ SvinefarmGr1/Scripts/app/farm-controller.js
@@ -0,0 +1,22 @@
+﻿angular.module('FarmApp', [])
+    .controller('FarmCtrl', function ($scope, $http) {
+
+        $scope.CreateFarm = function () {
+
+            $http({
+                method: 'POST',
+                url: 'api/farm',
+                data: "=" + $scope.name,
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).success(function (data, status, headers, config) {
+                $scope.title = "Hurray";
+            }).error(function (data, status, headers, config) {
+                $scope.title = "Oops... something went wrong";
+
+            });
+        };
+
+
+    });
+
+
