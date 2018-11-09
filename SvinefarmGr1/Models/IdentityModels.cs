5726c147b706f98e1faed143c700aea80af9e237
Dennis Dupont <TheInsanitor@hotmail.com>
2018-11-09 11:10:54 +0100

    We did ALOT, we're sorry...

--- SvinefarmGr1/Models/IdentityModels.cs
+++ SvinefarmGr1/Models/IdentityModels.cs
@@ -14,13 +14,13 @@ namespace SvinefarmGr1.Models
     {
         public string Hometown { get; set; }
 
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
+        //public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        //{
+        //    // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+        //    var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+        //    // Add custom user claims here
+        //    return userIdentity;
+        //}
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
